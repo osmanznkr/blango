be79aacebf0c56205a2239aaefc9af45a239a13e
Osman UZUNOKUR <osman.uzunokur@abclojistik.com>
2024-11-11 17:52:17 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 import blog.views
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+
+print(f"Time zone: {settings.TIME_ZONE}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
